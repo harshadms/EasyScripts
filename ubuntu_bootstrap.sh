0cf7b2d20cfc2ddd6f0725e33bf704dd81a526ff
harshadms <11594216+harshadms@users.noreply.github.com>
2025-05-17 19:00:10 +0200

    Create ubuntu_bootstrap.sh

---
+++ ubuntu_bootstrap.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install git
+
+cd Downloads
+wget https://dl.audiorelay.net/setups/linux/audiorelay-0.27.5.deb
+sudo dpkg -i audiorelay-0.27.5.deb
+
+# Brave
+curl -fsS https://dl.brave.com/install.sh | sh
