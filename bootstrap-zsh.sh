9525d645d162cc98a04a89591c645a73c5664eec
harshadms <11594216+harshadms@users.noreply.github.com>
2025-11-12 10:52:24 +0100

    Modify bootstrap-zsh.sh for .zshrc and prompt path
    Updated .zshrc creation to append configurations and changed the prompt installation path.

--- bootstrap-zsh.sh
+++ bootstrap-zsh.sh
@@ -13,8 +13,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
 
 # Create .zshrc with SSH locale fixes
-cat > ~/.zshrc << 'EOL'
-export ZSH="$HOME/.oh-my-zsh"
+echo "export ZSH="$HOME/.oh-my-zsh" 
 plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
 
 # Fix SSH character rendering
@@ -22,12 +21,12 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-source $ZSH/oh-my-zsh.sh
-[ -f ~/.zsh_prompt ] && source ~/.zsh_prompt
-EOL
+source $ZSH/oh-my-zsh.sh" >> ~/.zshrc
+
+#[ -f ~/.zsh_prompt ] && source ~/.zsh_prompt
 
 # Install custom prompt (replace URL with your actual prompt file)
-curl -L https://raw.githubusercontent.com/harshadms/EasyScripts/refs/heads/master/zsh_prompt -o ~/.zsh_prompt
+curl -L https://raw.githubusercontent.com/harshadms/EasyScripts/refs/heads/master/zsh_prompt -o $ZSH/themes/robbyrussell.zsh-theme
 
 # Set as default shell
 sudo chsh -s $(which zsh) $(whoami)
